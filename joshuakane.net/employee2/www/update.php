d9295a74ed333f999bbbc9bfc17833c3ec4842a8
Joshua K. Stewart <30303947+jks403@users.noreply.github.com>
2022-10-13 17:24:32 -0700

    Update update.php
    Update sends to the database ... No need to Filter_SANITIZE_INT
    This form takes all the fields from Edit.php and sends the fields to the update.php file. The variables are carried over and filtered. 
    Then the database is updated and we prepare the query and echo out the results

--- joshuakane.net/employee2/www/update.php
+++ joshuakane.net/employee2/www/update.php
@@ -1,16 +1,45 @@
 <?php
-
 require_once('../db_config.php');
 
-$query = "UPDATE teachers
-            SET firstname = 'Adam', surname = 'Carter'
-            WHERE id = 3";
-if($db_connection-> exec($query))
-{
-    echo"success";
-}
 
-else
-{
-    echo "failed";
+
+if(!isset($_POST['updateRecord'])){
+    header('Location: edit.php');
+    die();
+}else{
+    if(!isset($_POST['id'])){
+        header('Location: edit.php');
+    die();
+    }else{
+        $id = filter_var($_POST['id']);
+        $height = filter_var($_POST['height']);
+        $title = filter_var($_POST['title']);
+        $author = filter_var($_POST['author']);
+        $genre = filter_var($_POST['genre']);
+        $publisher = filter_var($_POST['publisher']);
+        
+        // now we do the query -->
+        $query = "UPDATE books 
+                  SET title = :title,
+                  author    = :author,
+                  genre     = :genre,
+                  height    = :height,
+                  publisher = :publisher
+                  WHERE id  = :id";
+                  
+                  $result = $db_conn ->prepare($query);
+                  $result ->execute([
+                            'title'  =>    $title,
+                            'author' =>    $author,
+                            'genre'  =>    $genre,
+                            'height' =>    $height,
+                            'publisher' => $publisher,
+                            'id'     =>    $id
+                      
+                      
+                      
+                      ]);
+
+    }
+    
 }
